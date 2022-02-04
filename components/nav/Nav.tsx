6b54ba74803e242a416d2c9a140ed2e9e0ab05a1
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-02-04 10:34:37 -0800

    Fixing resolutions

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -9,6 +9,7 @@ interface Props {
 export const Nav: React.FC<Props> = ({ page }) => {
 
     const pages = [
+        { page: "/", title: "Home" },
         { page: "/aboutme", title: "About Me" },
         { page: "/projects", title: "Projects" },
         { page: "/skills", title: "Skills" },
@@ -17,24 +18,24 @@ export const Nav: React.FC<Props> = ({ page }) => {
 
     return (
         <nav className='h-24 w-full absolute left-0 top-0 flex justify-center items-center z-50'>
+            <div className='absolute left-0'>
+                <Link href={"/"} scroll={false} passHref>
+                    <button className='w-fit'>
+                        <Logo />
+                    </button>
+                </Link>
+            </div>
             <div className="w-[1200px] flex justify-center items-center relative">
 
-                <div className='absolute -left-7'>
-                    <Link href={"/"} scroll={false} passHref>
-                        <button className='w-fit'>
-                            <Logo />
-                        </button>
-                    </Link>
-                </div>
 
                 <div className='flex items-center justify-between text-2xl text-grey w-[600px]'>
 
                     {
                         pages.map((item, i) => {
                             return (
-                                    <Link href={item.page} scroll={false} passHref key={`nav-${i}`}>
-                                        <p className={`${page === item.page && "text-light underline"} cursor-pointer`}>{item.title}</p>
-                                    </Link>
+                                <Link href={item.page} scroll={false} passHref key={`nav-${i}`}>
+                                    <p className={`${page === item.page && "text-light underline"} cursor-pointer`}>{item.title}</p>
+                                </Link>
 
                             )
                         })
